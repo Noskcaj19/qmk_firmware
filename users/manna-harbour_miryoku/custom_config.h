0dbc78d80b0ed6676c54920da96af857b4d32702
Noskcaj <kcajdev@gmail.com>
2022-09-21 11:29:44 -0400

    Add combos

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -10,30 +10,42 @@
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 #define TAPPING_TOGGLE 2
-#define COMBO_COUNT 2
+#define COMBO_COUNT 8
+#define COMBO_MUST_PRESS_IN_ORDER_PER_COMBO
+#define COMBO_TERM_PER_COMBO
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 
 #define EE_HANDS
 
 #define MIRYOKU_LAYERMAPPING_GAME_NUM LAYOUT_split_3x6_3
 #define MIRYOKU_LAYER_GAME_NUM \
     KC_NO,        KC_6,    KC_7,    KC_8,    KC_9,      KC_0,                   U_NA,   U_NA,    U_NA,    U_NA,    RESET,   KC_NO, \
-    MO(U_GAME_NUM), KC_1,    KC_2,    KC_3,    KC_4,      KC_5,                   U_NA,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, \
-    KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_GRAVE,  KC_NO,                  U_NA,   U_NA,    U_NA,    KC_ALGR, U_NA,    KC_NO, \
+    MO(U_GAME_NUM), KC_1,  KC_2,    KC_3,    KC_4,      KC_5,                   U_NA,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, \
+    KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_GRV,    KC_NO,                  U_NA,   U_NA,    U_NA,    KC_ALGR, U_NA,    KC_NO, \
                                           KC_DOT,  KC_0,   KC_MINS,     KC_NO,  KC_NO ,  KC_NO
 
 #define MIRYOKU_LAYERMAPPING_GAME LAYOUT_split_3x6_3
 #define MIRYOKU_LAYER_GAME \
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRAVE, \
-     MO(U_GAME_NUM), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV, \
+     MO(U_GAME_NUM),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_GAME),  \
                                           KC_LCTL, KC_SPC, KC_LALT,      KC_ENT, KC_BSPC, KC_LGUI
 
+#define MIRYOKU_LAYERMAPPING_BASE LAYOUT_split_3x6_3
+#define MIRYOKU_LAYER_BASE \
+     XXXXXXX,     KC_Q,             KC_W,          KC_E,         KC_R,         KC_T,  /*|*/   KC_Y, KC_U,         KC_I,         KC_O,           KC_P,                  KC_BSLS, \
+     XXXXXXX,     LGUI_T(KC_A),     LALT_T(KC_S),  LCTL_T(KC_D), LSFT_T(KC_F), KC_G,  /*|*/   KC_H, LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_SCLN),       KC_QUOT,  \
+     TD(TD_GAME), LT(U_BUTTON, KC_Z), ALGR_T(KC_X), KC_C,        KC_V,         KC_B,  /*|*/   KC_N, KC_M,         KC_COMM,      ALGR_T(KC_DOT), LT(U_BUTTON, KC_SLSH), TD(TD_GAME),  \
+                        LT(U_MEDIA, KC_ESC), LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB),             LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), LT(U_FUN, KC_DEL)
+
+#if false
 #define MIRYOKU_LAYERMAPPING_BASE LAYOUT_split_3x6_3
 #define MIRYOKU_LAYER_BASE \
      XXXXXXX,     KC_Y,             KC_C,          KC_L,         KC_M,         KC_K,  /*|*/   KC_Z, KC_F,         KC_U,         KC_COMM,        KC_QUOT,          XXXXXXX, \
      XXXXXXX,     LGUI_T(KC_I),     LALT_T(KC_S),  LCTL_T(KC_R), LSFT_T(KC_T), KC_G,  /*|*/   KC_P, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_A),   LGUI_T(KC_O),     XXXXXXX,  \
      TD(TD_GAME), LT(U_BUTTON, KC_Q), ALGR_T(KC_V), KC_W,        KC_D,         KC_J,  /*|*/   KC_B, KC_H,         KC_SLSH,      ALGR_T(KC_DOT), LT(U_BUTTON, KC_X), TD(TD_GAME),  \
                         LT(U_MEDIA, KC_ESC), LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB),             LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), LT(U_FUN, KC_DEL)
+#endif
 
 #define MIRYOKU_LAYERMAPPING_TAP LAYOUT_split_3x6_3
 #define MIRYOKU_LAYER_TAP \
